a274cedc31444d6e516e771f8c7a8630c73359ae
prakapniou <prakapniou@gmail.com>
2024-12-19 21:37:35 +0300

    CountingDuplicates completed

---
+++ CountingDuplicates/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Write a function that will return the count 
+ * of distinct case-insensitive alphabetic characters and numeric digits 
+ * that occur more than once in the input string. 
+ * The input string can be assumed to contain only alphabets 
+ * (both uppercase and lowercase) and numeric digits.*/
+
+static int DuplicateCount(string str)
+{
+    var upperStr=str.ToUpper();
+    int i = default;
+
+    while(upperStr.Length>0)
+    {
+        var length=upperStr.Length;
+        var firstSymbol=upperStr[0].ToString();
+        upperStr=upperStr.Replace(firstSymbol,string.Empty);
+        var newLength=upperStr.Length;
+        if (Math.Abs(length-newLength)>1) i++;
+    }
+
+    return i;
+}
+
+Console.WriteLine(DuplicateCount("AAABBDDFF")); 
