b82e75dedc69866d5cee008ce239ce290bf61247
prakapniou <prakapniou@gmail.com>
2024-12-19 19:50:29 +0300

    StringEndsWith completed

---
+++ StringEndsWith/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Complete the solution so that it returns
+ * true if the first argument(string) passed in ends 
+ * with the 2nd argument (also a string).*/
+
+static bool Solution(string str, string ending)=>str.EndsWith(ending);
+
+Console.WriteLine(Solution("abc","bc"));
